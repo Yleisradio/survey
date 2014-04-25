5674d4d4e0a0c317534b5b34e91e36ec1ac0a076
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2014-04-25 10:51:04 +0300

    Removed unnecessary label

--- protected/views/survey/update.php
+++ protected/views/survey/update.php
@@ -5,13 +5,6 @@
 
 <h1><?php echo Yii::t('admin', 'survey.update.heading'); ?></h1>
 
-<div class="form-horizontal">
-    <div class="form-group">
-        <div class="col-sm-2 control-label">
-            <?php echo Yii::t('admin', 'survey.active'); ?>
-        </div>
-    </div>
-</div>
 <?php
 $this->renderPartial('_form', array('model' => $model));
 
